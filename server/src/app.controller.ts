cf459e8a231c40c12406a36182960d9a2a82886f
code-bullit <bigbronco0983@gmail.com>
2020-07-28 19:20:27 +0530

    Initial Commit

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller,Get } from '@nestjs/common';
-import { AppService } from './app.service'
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private AppService: appService){}
+  constructor(private appService: AppService) {}
 
-    @Get()
-    async getAll(): Promise<string> {
-        return this.appService.getAll();
-    }
+  @Get()
+  async getAll(): Promise<string> {
+    return this.appService.getAll();
+  }
 }
